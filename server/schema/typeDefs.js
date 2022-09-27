83ff562b2bd00ce65ce932843b51c1a2f3b8475f
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-09-27 00:14:01 -0500

    add user typedef

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -1,6 +1,13 @@
 const {gql} = require('apollo-server-express');
 
-const typeDefs = gql``;
+const typeDefs = gql`
+    type User {
+        _id: ID!
+        username: String
+        bookCount: Int
+        savedBooks [Book]
+    }
+`;
 
 
 module.exports = typeDefs;
