0f3f2dfe31c9b03ad5e502f9c0e5fbe9c3a9e42d
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-09-27 00:16:54 -0500

    add book typedef

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -4,8 +4,18 @@ const typeDefs = gql`
     type User {
         _id: ID!
         username: String
+        email: String
         bookCount: Int
         savedBooks [Book]
+    }
+
+    type Book {
+        bookId: ID!
+        authors: [String]
+        description: String
+        title: String!
+        image: String
+        link: String
     }
 `;
 
