924a064a4ff5dea7a9bb0977bbfacc1e4f5896c5
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-09-27 00:31:45 -0500

    add query

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -2,6 +2,8 @@ const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
-const resolvers = {};
+const resolvers = {
+    
+};
 
 module.exports = resolvers;
